eb50342f0b88c0ee369c8bb2bdfd94149a9849e0
Anamika mishra <mishraanamika8146@gmail.com>
2026-04-10 10:54:32 +0530

    Time: 0 ms (100%), Space: 11.5 MB (77.86%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -3,11 +3,36 @@ public:
     void sortColors(vector<int>& nums) {
        int n = nums.size();
 
-       for(int i=0 ; i<n-1 ; i++){
-        int j=i+1;
-        while(j>=1 && nums[j]<nums[j-1]){
-            swap(nums[j],nums[j-1]);
-                j--;   
+        // int low = 0 , mid = 0 , high = n-1;
+
+        // for (int i=1 ; i<n ; i++){
+        //     if(nums[low]==0 && nums[low]<nums[high]){
+        //         swap(nums[low], nums[high]);
+        //         high--;
+        //     }
+        //     else if(nums[low]==nums[high]) {
+        //         low++;
+        //         high++;
+        //     }
+        // }
+    //    for(int i=0 ; i<n-1 ; i++){
+    //     int j=i+1;
+    //     while(j>=1 && nums[j]<nums[j-1]){
+    //         swap(nums[j],nums[j-1]);
+    //             j--;   
+    //     }
+    // }
+
+    int i=0 , j=0  , k = n-1;
+    while(j<=k){
+        if(nums[j]==1) j++;
+        else if (nums[j]==2) {
+            swap(nums[j], nums[k]);
+            k--;
+        }
+        else{
+            swap(nums[i],nums[j]);
+            i++; j++;
         }
     }
     nums;
