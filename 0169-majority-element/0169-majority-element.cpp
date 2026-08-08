d38e4f27b21bb22eaf4b45ba5a10800c2dd4a70f
Anamika mishra <mishraanamika8146@gmail.com>
2026-08-08 22:28:58 +0530

    Time: 0 ms (100%), Space: 28.2 MB (41.6%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -10,23 +10,36 @@ public:
     //         if(count>n/2) return arr[i];
     //    }
     //    return -1;
-    sort(arr.begin(),arr.end());
+    // sort(arr.begin(),arr.end());
 
-    int freq = 1, ans=arr[0];
+    // int freq = 1, ans=arr[0];
 
-    for(int i=1 ; i<n ; i++){
-        if(arr[i]==arr[i-1]){
-            freq++;
-        }
-        else{
-            freq = 1;
-            ans = arr[i];
-        }
-        if(freq>n/2){
-            return ans;
-        }
+    // for(int i=1 ; i<n ; i++){
+    //     if(arr[i]==arr[i-1]){
+    //         freq++;
+    //     }
+    //     else{
+    //         freq = 1;
+    //         ans = arr[i];
+    //     }
+    //     if(freq>n/2){
+    //         return ans;
+    //     }
 
-    }
+        int freq = 0 , ans = 0;
+
+        for(int i = 0 ; i<n ; i++){
+
+            if(freq==0) ans = arr[i];
+
+            if(arr[i]==ans) freq++;
+            
+            else freq--;
+        }
+    // }
+
+    
     return ans;
     }
 };
+
