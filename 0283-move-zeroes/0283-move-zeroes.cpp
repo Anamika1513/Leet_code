ac49129cedd0cfb727757d86fd6d43768423aa76
Anamika mishra <mishraanamika8146@gmail.com>
2026-09-17 21:08:04 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.4%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,13 +3,23 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
 
-        for(int i=0 ; i<n-1 ; i++){
-            
-            for(int j=0 ; j<n-1-i ;j++){
-                if(nums[j]==0){
-                    swap(nums[j],nums[j+1]);
-                }
+        if(n==1) return;
+
+        // for(int i=0 ; i<n-1 ; i++){ 
+        //     for(int j=0 ; j<n-1-i ;j++){
+        //         if(nums[j]==0){
+        //             swap(nums[j],nums[j+1]);
+        //         }
+        //     }
+        // }
+
+        int j=0;
+        for(int i=0 ; i<n ; i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
             }
         }
+        return;
     }
 };
